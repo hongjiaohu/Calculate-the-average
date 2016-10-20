ab43a28a9f6d94ac0aa6ec0ee850ca18043670a4
hongjiaohu <h.hu@sjasd.ca>
2016-10-20 10:05:45 -0500

    A2,P2
    Calculate the average of marks entered by a user

---
+++ frmCalculate the average.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Calculate_the_average
+{
+    partial class frmCalculatetheaverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtThirdNumber = new System.Windows.Forms.TextBox();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.txtFourthNumber = new System.Windows.Forms.TextBox();
+            this.txtFifthNumber = new System.Windows.Forms.TextBox();
+            this.lblFirstNumber = new System.Windows.Forms.Label();
+            this.lblSecondNumber = new System.Windows.Forms.Label();
+            this.lblThirdNumber = new System.Windows.Forms.Label();
+            this.lblFourthNumber = new System.Windows.Forms.Label();
+            this.lblFifthNumber = new System.Windows.Forms.Label();
+            this.btnCalculateAverage = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblfirst = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtThirdNumber
+            // 
+            this.txtThirdNumber.Location = new System.Drawing.Point(180, 311);
+            this.txtThirdNumber.Name = "txtThirdNumber";
+            this.txtThirdNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtThirdNumber.TabIndex = 0;
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(180, 197);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstNumber.TabIndex = 1;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(180, 256);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtSecondNumber.TabIndex = 2;
+            // 
+            // txtFourthNumber
+            // 
+            this.txtFourthNumber.Location = new System.Drawing.Point(180, 368);
+            this.txtFourthNumber.Name = "txtFourthNumber";
+            this.txtFourthNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtFourthNumber.TabIndex = 3;
+            // 
+            // txtFifthNumber
+            // 
+            this.txtFifthNumber.Location = new System.Drawing.Point(180, 428);
+            this.txtFifthNumber.Name = "txtFifthNumber";
+            this.txtFifthNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtFifthNumber.TabIndex = 4;
+            // 
+            // lblFirstNumber
+            // 
+            this.lblFirstNumber.AutoSize = true;
+            this.lblFirstNumber.Location = new System.Drawing.Point(45, 196);
+            this.lblFirstNumber.Name = "lblFirstNumber";
+            this.lblFirstNumber.Size = new System.Drawing.Size(117, 13);
+            this.lblFirstNumber.TabIndex = 5;
+            this.lblFirstNumber.Text = "Please Enter A Number";
+            // 
+            // lblSecondNumber
+            // 
+            this.lblSecondNumber.AutoSize = true;
+            this.lblSecondNumber.Location = new System.Drawing.Point(45, 256);
+            this.lblSecondNumber.Name = "lblSecondNumber";
+            this.lblSecondNumber.Size = new System.Drawing.Size(117, 13);
+            this.lblSecondNumber.TabIndex = 6;
+            this.lblSecondNumber.Text = "Please Enter A Number";
+            // 
+            // lblThirdNumber
+            // 
+            this.lblThirdNumber.AutoSize = true;
+            this.lblThirdNumber.Location = new System.Drawing.Point(45, 311);
+            this.lblThirdNumber.Name = "lblThirdNumber";
+            this.lblThirdNumber.Size = new System.Drawing.Size(117, 13);
+            this.lblThirdNumber.TabIndex = 7;
+            this.lblThirdNumber.Text = "Please Enter A Number";
+            // 
+            // lblFourthNumber
+            // 
+            this.lblFourthNumber.AutoSize = true;
+            this.lblFourthNumber.Location = new System.Drawing.Point(45, 368);
+            this.lblFourthNumber.Name = "lblFourthNumber";
+            this.lblFourthNumber.Size = new System.Drawing.Size(117, 13);
+            this.lblFourthNumber.TabIndex = 8;
+            this.lblFourthNumber.Text = "Please Enter A Number";
+            // 
+            // lblFifthNumber
+            // 
+            this.lblFifthNumber.AutoSize = true;
+            this.lblFifthNumber.Location = new System.Drawing.Point(45, 428);
+            this.lblFifthNumber.Name = "lblFifthNumber";
+            this.lblFifthNumber.Size = new System.Drawing.Size(117, 13);
+            this.lblFifthNumber.TabIndex = 9;
+            this.lblFifthNumber.Text = "Please Enter A Number";
+            // 
+            // btnCalculateAverage
+            // 
+            this.btnCalculateAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculateAverage.Location = new System.Drawing.Point(440, 177);
+            this.btnCalculateAverage.Name = "btnCalculateAverage";
+            this.btnCalculateAverage.Size = new System.Drawing.Size(173, 56);
+            this.btnCalculateAverage.TabIndex = 10;
+            this.btnCalculateAverage.Text = "Calculate Average";
+            this.btnCalculateAverage.UseVisualStyleBackColor = true;
+            this.btnCalculateAverage.Click += new System.EventHandler(this.btnCalculateAverage_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(318, 246);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(447, 255);
+            this.lblResult.TabIndex = 11;
+            // 
+            // lblfirst
+            // 
+            this.lblfirst.AutoSize = true;
+            this.lblfirst.Font = new System.Drawing.Font("Monotype Corsiva", 24F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfirst.Location = new System.Drawing.Point(173, 64);
+            this.lblfirst.Name = "lblfirst";
+            this.lblfirst.Size = new System.Drawing.Size(385, 39);
+            this.lblfirst.TabIndex = 12;
+            this.lblfirst.Text = "calculate the average of marks";
+            // 
+            // frmCalculatetheaverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 611);
+            this.Controls.Add(this.lblfirst);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculateAverage);
+            this.Controls.Add(this.lblFifthNumber);
+            this.Controls.Add(this.lblFourthNumber);
+            this.Controls.Add(this.lblThirdNumber);
+            this.Controls.Add(this.lblSecondNumber);
+            this.Controls.Add(this.lblFirstNumber);
+            this.Controls.Add(this.txtFifthNumber);
+            this.Controls.Add(this.txtFourthNumber);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.txtThirdNumber);
+            this.Name = "frmCalculatetheaverage";
+            this.Text = "frmCalculatetheaverage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtThirdNumber;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.TextBox txtFourthNumber;
+        private System.Windows.Forms.TextBox txtFifthNumber;
+        private System.Windows.Forms.Label lblFirstNumber;
+        private System.Windows.Forms.Label lblSecondNumber;
+        private System.Windows.Forms.Label lblThirdNumber;
+        private System.Windows.Forms.Label lblFourthNumber;
+        private System.Windows.Forms.Label lblFifthNumber;
+        private System.Windows.Forms.Button btnCalculateAverage;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblfirst;
+    }
+}
+
---
+++ frmCalculate the average.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculate_the_average
+{
+    public partial class frmCalculatetheaverage : Form
+    {
+        public frmCalculatetheaverage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateAverage_Click(object sender, EventArgs e)
+        {
+            /**
+
+ *
+
+ * Name: Hongjiao Hu
+
+ * Teacher: Mr. Hardman
+ 
+ * Assignment #2, Program #2
+
+ * Date Last Modified: 10/19/2016
+
+ *
+
+ */
+            //using user inpuut number
+            double Firstnume = Convert.ToDouble(txtFirstNumber.Text);
+            double Secondnume = Convert.ToDouble(txtSecondNumber.Text);
+            double Thirdnume = Convert.ToDouble(txtThirdNumber.Text);
+            double Fourthnume = Convert.ToDouble(txtFourthNumber.Text);
+            double Fifthnume = Convert.ToDouble(txtFifthNumber.Text);
+
+            //using arithmic operator for caculation
+            double average = (Firstnume + Secondnume + Thirdnume + Fourthnume + Fifthnume)/5;
+
+            //Dispaly all caculation to the user
+            lblResult.Text = string.Format("{0,10}{1,15}\n{2,10}{3,15}\n{4,10}{5,15}\n{6,10}{7,15}\n{8,10}{9,15}\n\n{10,10}{11,15}",
+                 "Grade 1:", Firstnume, " Grade 2:", Secondnume, "Grade 3:", Thirdnume, "Grade 4:", Fourthnume, "Grade 5:", Fifthnume, "Average:",average );
+
+
+
+
+        }
+    }
+}
